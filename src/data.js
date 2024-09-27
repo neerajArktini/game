55ef2622e4416392ec105dcc2350b00415b05125
neerajArktini <neeraj.pant@arktiniinnovations.com>
2024-09-27 22:24:06 +0530

    feat(App): added new feature for game

---
+++ src/data.js
@@ -0,0 +1,31 @@
+export const gamesLevel = [
+    [
+      { commonText: "tap on", colorText: "green", textColor: "brown", answer: ["green"] },
+      { commonText: "tap on", colorText: "red", textColor: "green", answer: ["red"] },
+      { commonText: "tap on", colorText: "brown", textColor: "red", answer: ["brown"] }
+    ],
+    [
+      { commonText: "tap on", colorText: "green", textColor: "brown", answer: ["green"] },
+      { commonText: "tap on", colorText: "red", textColor: "green", answer: ["red"] },
+      { commonText: "tap on", colorText: "brown", textColor: "red", answer: ["green"] },
+      { commonText: "tap on", colorText: "red", textColor: "brown", answer: ["red"] }
+    ],
+    [
+      { commonText: "don't tap on", colorText: "green", textColor: "green", answer: ["red", "brown"] },
+      { commonText: "don't tap on", colorText: "red", textColor: "green", answer: ["green", "brown"] },
+      { commonText: "don't tap on", colorText: "brown", textColor: "red", answer: ["green", "red"] }],
+    [
+      { commonText: "don't tap on", colorText: "green", textColor: "brown", answer: ["red", "brown"] },
+      { commonText: "don't tap on", colorText: "red", textColor: "green", answer: ["green", "brown"] },
+      { commonText: "don't tap on", colorText: "brown", textColor: "red", answer: ["green", "red"] },
+      { commonText: "don't tap on", colorText: "red", textColor: "brown", answer: ["green", "brown"] }
+    ],
+    [
+      { commonText: "tap on", colorText: "red", textColor: "green", answer: ["red"] },
+      { commonText: "don't tap on", colorText: "green", textColor: "brown", answer: ["red", "brown"] },
+      { commonText: "tap on", colorText: "brown", textColor: "red", answer: ["green"] },
+      { commonText: "don't tap on", colorText: "red", textColor: "green", answer: ["green", "brown"] },
+      { commonText: "tap on", colorText: "red", textColor: "brown", answer: ["red"] },
+      { commonText: "don't tap on", colorText: "brown", textColor: "red", answer: ["green", "red"] },
+    ]
+  ]
